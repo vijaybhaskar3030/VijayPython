893b2bee32fb08ee26dfa35002102b9aa1da69ef
vijay <vijaybhaskar3030@gmail.com>
2018-11-14 11:51:20 +0530

    Added first python files

---
+++ Second.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 18:17:29 2018
+
+@author: REIU257
+"""
+
+length =1.10
+width = 2.20
+area = length*width
+print ("the length is:", length)
+print ("the width is:", width)
+print ("the area is:", area)
+print ("the length is:", length,"the width is:",width,"and the area is:",area)
+
+
+the_world_is_flat = True
+if the_world_is_flat:
+print("Be careful not to fall off!")
---
+++ hello.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 18:13:52 2018
+
+@author: REIU257
+"""
+
+print("Hello Anaconda")
